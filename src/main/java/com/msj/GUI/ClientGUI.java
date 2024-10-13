20e415fff2cb9524b1df030cfc61996261daf272
rori_badwiki <rorisangmakganaiv@gmail.com>
2024-10-13 11:31:13 +0200

    Working on a second rendition of the Client Server chat application and working with Java Networking, this is the ADP class task, and this is the initial commit

---
+++ src/main/java/com/msj/GUI/ClientGUI.java
@@ -0,0 +1,11 @@
+package com.msj.GUI;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
+import java.util.*;
+
+public class ClientGUI extends JFrame{
+    
+}
---
+++ src/main/java/com/msj_client/ADP2Motor_Sport_JournalClient.java
@@ -0,0 +1,7 @@
+package com.msj_client;
+public class ADP2Motor_Sport_JournalClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
